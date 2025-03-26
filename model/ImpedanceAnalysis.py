f7eda5961d1b9791738362ddbee9880bbb974074
Justin Bahr <bahr.j@northeastern.edu>
2025-03-26 11:35:14 -0400

    Added calc_cap() to ImpedanceAnalysis.py and two channel threshold to RealTimeAnalysis.py

--- model/ImpedanceAnalysis.py
+++ model/ImpedanceAnalysis.py
@@ -1,30 +1,59 @@
 import statistics
 import random
+import math
 
 class ImpedanceAnalysis:
-    def __init__(self, impedance_data, numChunks = 10):
-        self.impedance_data = impedance_data
-        self.total_length = len(self.impedance_data)
+    def __init__(self, data, numChunks = 10):
+        self.low = data[0]
+        self.high = data[1]
+        self.low_length = len(self.low)
+        self.high_length = len(self.high)
         self.numChunks = numChunks
-        self.avg_list = []
-        self.imp_list = []
+        self.avg_low_list = []
+        self.avg_high_list = []
+        self.imp_low_list = []
+        self.imp_high_list = []
+        self.cap_list = []
+        self.fH = 2390 # high frequency probe
+        self.fL = 250 # low frequency probe
 
     def run(self):
         self.chunk_avg()
         self.calc_imp()
-        
-        return self.imp_list
+        self.calc_cap()
 
     def chunk_avg(self):
-        chunk_size = self.total_length // self.num_chunks
-        for i in range(self.num_chunks-1):
-            self.avg_list.append(statistics.mean(self.impedance_data[i:i+chunk_size-1]))
-        self.avg_list.append(statistics.mean(self.impedance_data[self.num_chunks-1:]))
+        # chunks low frequency data into average bins
+        low_chunk_size = self.low_length // self.numChunks
+        for i in range(self.numChunks-1):
+            self.avg_low_list.append(statistics.mean(self.low[i:i+low_chunk_size-1]))
+        self.avg_low_list.append(statistics.mean(self.low[self.numChunks-1:]))
+
+        # chunks high frequency data into average bins
+        high_chunk_size = self.high_length // self.numChunks
+        for i in range(self.numChunks - 1):
+            self.avg_high_list.append(statistics.mean(self.high[i:i + high_chunk_size - 1]))
+        self.avg_high_list.append(statistics.mean(self.high[self.numChunks - 1:]))
 
     def calc_imp(self):
+        # saves the transimpedance amplifier resistance gain times the input voltage
         rf_vin = 5600*200
-        for i in range(self.num_chunks):
-            self.imp_list.append(rf_vin/self.avg_list[i])
+
+        # computes and saves impedances at the low frequency
+        for i in range(self.numChunks):
+            self.imp_low_list.append(rf_vin/self.avg_low_list[i])
+
+        # computes and stores impedances at the high frequency
+        for i in range(self.numChunks):
+            self.imp_high_list.append(rf_vin/self.avg_high_list[i])
+
+    def calc_cap(self):
+        # saves a frequency ratio for future calculations
+        fR = math.sqrt(self.fH**2-self.fL**2) / (2*math.pi*self.fH*self.fL)
+
+        # computes and saves capacitances at each time chunk
+        for i in range(self.numChunks):
+            self.cap_list.append(fR / math.sqrt(self.imp_low_list[i]**2 - self.imp_high_list[i]**2))
 
 def generateRandomVal(base):
     noise = random.random() * 40
--- model/RealTimeAnalysis.py
+++ model/RealTimeAnalysis.py
@@ -2,15 +2,24 @@ class RealTimeAnalysis:
   def __init__(self, data):
     self.low = data[0]
     self.high = data[1]
-    self.threshold = 275 # experimentally determine a good value
+    self.low_threshold = 240 # experimentally determine a good value
+    self.high_threshold = 250  # experimentally determine a good value
     self.significant = 50 # can tweak this
 
   def checkThreshold(self):
-    num_sig = 0
-    for d in self.data:
-      if d > self.threshold:
-        num_sig = num_sig + 1
-    if num_sig >= self.significant:
+    # counts the number of low frequency readings over the low threshold
+    num_sig_low = 0
+    for d in self.low:
+      if d > self.low_threshold:
+        num_sig_low = num_sig_low + 1
+
+    # counts the number of high frequency readings over the high threshold
+    num_sig_high = 0
+    for d in self.high:
+      if d > self.high_threshold:
+        num_sig_high = num_sig_high + 1
+
+    if num_sig_low+num_sig_high >= self.significant:
       return True
     else:
       return False
