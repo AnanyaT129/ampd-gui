66561c3cee2db2256c9aa750d2e30133b8bb97bf
Ananya Tadigadapa <anatadig@gmail.com>
2025-04-06 19:03:52 -0400

    LED status to set color to what we want

--- model/LEDcontrol.py
+++ model/LEDcontrol.py
@@ -1,7 +1,18 @@
+from enum import Enum
 import os
 import time
 import neopixel
 import board
+
+class LEDStatus(Enum):
+    OFF = (0, 0, 0)
+    READY = (255, 0, 0)
+    RUNNING = (255, 255, 0)
+    ERROR = (0, 255, 0)
+
+    PLASTIC_DETECTED = (0, 255, 0)
+    PLASTIC_NOT_DETECTED = (255, 0, 0)
+    PLASTIC_CONTENT_UNSURE = (255, 255, 0)
 
 class LEDScontrol:
     def __init__(self, num_pixels, brightness=1.0, auto_write=False, pin=board.D18):
@@ -34,3 +45,20 @@ class LEDScontrol:
     def set_brightness(self, brightness):
         """Set global brightness (0.0 to 1.0)."""
         self.strip.brightness = brightness
+    
+    def set_lighting(self, status: LEDStatus):
+        match(status):
+            # status of the device turns first light on
+            case LEDStatus.OFF | LEDStatus.READY | LEDStatus.RUNNING | LEDStatus.ERROR:
+                print(status.value)
+                self.clear()
+                self.set_pixel(0, status.value)
+            
+            # plastic result turns everything but the first light on
+            case LEDStatus.PLASTIC_DETECTED | LEDStatus.PLASTIC_NOT_DETECTED | LEDStatus.PLASTIC_CONTENT_UNSURE:
+                print(status.value)
+                self.clear()
+                self.fill(status.value)
+                self.set_pixel(0, (0, 0, 0))
+        
+        self.show()
